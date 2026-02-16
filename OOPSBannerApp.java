5797a45958bbc7bd4152f2a8f95c9a4f4737a5d6
tp1792 <tp1792@srmist.edu.in>
2026-02-16 14:11:09 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // This prints "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
